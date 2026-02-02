657cd4394d430335443cb5b872a541c1572c4b2e
carlos-tveit <carlostveit@icloud.com>
2026-02-02 17:08:26 +0100

    Initial commit with clean structure

---
+++ MyClubApp/MyClubApp/MyClubAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyClubAppApp.swift
+//  MyClubApp
+//
+//  Created by Carlos Diaz Tveit on 02/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct MyClubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
